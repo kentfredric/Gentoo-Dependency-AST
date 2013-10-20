c90c528ed062c175482cf902bd4f8030e14f9e55
Kent Fredric <kentfredric@gmail.com>
2013-10-21 05:30:29 +1300

    Node/Group/Or.pm: Add Abstract

--- lib/Gentoo/Dependency/AST/Node/Group/Or.pm
+++ lib/Gentoo/Dependency/AST/Node/Group/Or.pm
@@ -3,6 +3,8 @@ use warnings;
 
 package Gentoo::Dependency::AST::Node::Group::Or;
 
+# ABSTRACT: A group of dependencies which only one is required
+
 use parent 'Gentoo::Dependency::AST::Node';
 
 1;
