3cd7a17448773525b50dc711e3711e175f423b74
Kent Fredric <kentfredric@gmail.com>
2013-10-21 05:27:54 +1300

    Node/Group/NotUse.pm: Add Abstract

--- lib/Gentoo/Dependency/AST/Node/Group/NotUse.pm
+++ lib/Gentoo/Dependency/AST/Node/Group/NotUse.pm
@@ -4,6 +4,8 @@ use warnings;
 
 package Gentoo::Dependency::AST::Node::Group::NotUse;
 
+# ABSTRACT: A group of dependencies that require a C<useflag> disabled to trigger require
+
 use parent 'Gentoo::Dependency::AST::Node::Group::Use';
 
 1;
