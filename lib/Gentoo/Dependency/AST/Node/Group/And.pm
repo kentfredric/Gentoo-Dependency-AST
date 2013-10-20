df45350dbe280b8a1038ba699f65a6b37a6406fe
Kent Fredric <kentfredric@gmail.com>
2013-10-21 05:30:19 +1300

    Node/Group/And.pm: Add Abstract

--- lib/Gentoo/Dependency/AST/Node/Group/And.pm
+++ lib/Gentoo/Dependency/AST/Node/Group/And.pm
@@ -3,6 +3,8 @@ use warnings;
 
 package Gentoo::Dependency::AST::Node::Group::And;
 
+# ABSTRACT: A Group of dependencies of which, all are required
+
 use parent 'Gentoo::Dependency::AST::Node';
 
 1;
