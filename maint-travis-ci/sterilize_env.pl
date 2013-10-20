1204e5d42251f404af5cf76fc691e742537e516c
Kent Fredric <kentfredric@gmail.com>
2013-10-21 02:26:46 +1300

    Build results of bf1c6d7 (on master)

--- maint-travis-ci/sterilize_env.pl
+++ maint-travis-ci/sterilize_env.pl
@@ -8,6 +8,24 @@ sub diag {
   print STDERR "\n";
 }
 
+sub safe_exec {
+  my ( $command, @params ) = @_;
+  diag("running $command @params");
+  my $exit = system( $command, @params );
+  if ( $exit != 0 ) {
+    my $low  = $exit & 0b11111111;
+    my $high = $exit >> 8;
+    warn "$command failed: $? $! and exit = $high , flags = $low";
+    if ( $high != 0 ) {
+      exit $high;
+    }
+    else {
+      exit 1;
+    }
+  }
+  return 1;
+}
+
 if ( not exists $ENV{STERILIZE_ENV} ) {
   diag('STERILIZE_ENV unset');
   exit 0;
@@ -24,8 +42,8 @@ for my $i (@INC) {
   next if $i !~ /site/;
   next if $i eq '.';
   diag( 'Sterilizing files in ' . $i );
-  system( 'find', $i, '-type', 'f', '-delete' );
+  safe_exec( 'find', $i, '-type', 'f', '-delete' );
   diag( 'Sterilizing dirs in ' . $i );
-  system( 'find', $i, '-depth', '-type', 'd', '-delete' );
+  safe_exec( 'find', $i, '-depth', '-type', 'd', '-delete' );
 }
 
